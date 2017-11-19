83f92daac7ffc6485acc894e0c734421cc83175d
Albert Hives <ahives@hotmail.com>
2017-11-19 09:43:39 -0800

    first bits of perf testing
    did some tweaking of the perf tests
    
    mono benchmark
    
    benchmark project for perf testing
    
    benchmark clean up
    
    clean up

---
+++ src/Machete.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Machete.Benchmark
+{
+    using BenchmarkDotNet.Running;
+    using HL7;
+    using HL7Schema.V26;
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<HL7Benchmark>();
+        }
+    }
+}
